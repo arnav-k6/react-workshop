2b84665c48b789ea7ff2ab1e56e4d7bea1acc4a6
arnav-k6 <arnavkapadia@gmail.com>
2025-11-15 13:37:58 -0800

    hello

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,11 @@
-// From the folder this file is in (./) look for an Item.css file.
-import "./Item.css";
-
-// We define <Item /> by writing a function called Item
-function Item(props) {
-  // props = { image: "...", name: "..." } because we did <Item image="..." name="..." />
-  // Components can only return one thing. That's why we wrapped everything in a div.
+function Item(){
   return (
-    <div className="item">
-      <img src={props.image} className="logo" />
-      <h2>{props.name}</h2>
+    <div>
+      <img className = "image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEZuT1CDTg-N6EMe8cAlh000s1VjUxeVbnKw&s">
+        <h2>Five guys</h2>
+      </img>
     </div>
-  );
+  )
 }
 
-// "export": make this file import-able by other files
-// "default": Item is the most important export of this file
-export default Item;
+export default Item
